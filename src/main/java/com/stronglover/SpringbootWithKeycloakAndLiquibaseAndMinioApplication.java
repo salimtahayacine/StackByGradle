a8dbaab8c02279c0a811bf64eeb3c764ec6ebe74
salimtahayacine <salimtahayacine@gmail.com>
2025-01-14 20:48:35 +0100

    src commit

---
+++ src/main/java/com/stronglover/SpringbootWithKeycloakAndLiquibaseAndMinioApplication.java
@@ -0,0 +1,13 @@
+package com.stronglover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithKeycloakAndLiquibaseAndMinioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithKeycloakAndLiquibaseAndMinioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stronglover/Springboot/with/keycloak/and/liquibase/and/minio/SpringbootWithKeycloakAndLiquibaseAndMinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stronglover.Springboot.with.keycloak.and.liquibase.and.minio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithKeycloakAndLiquibaseAndMinioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
